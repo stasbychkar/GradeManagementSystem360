b532d50fed8b462002bb98bdb1afe785023a25bf
Ilham <164689826+Seascavenger@users.noreply.github.com>
2025-05-23 23:04:25 -0700

    Add files via upload

---
+++ UI/EditButtonEditor.java
@@ -0,0 +1,50 @@
+// This class lets the "Edit" button in a JTable cell open the EditStudentDialog
+// and update the table with new values if the user clicks Save.
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class EditButtonEditor extends DefaultCellEditor {
+    private JButton button;
+    private JTable table;
+    private int row;
+
+    // Make the Edit button
+    public EditButtonEditor(JCheckBox checkBox, JTable table) {
+        super(checkBox);
+        this.table = table;
+        button = new JButton("Edit");
+        
+        // Stop editing when button is clicked
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                fireEditingStopped();
+            }
+        });
+    }
+
+    // Show the button in the cell
+    public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+        this.row = row;
+        return button;
+    }
+
+    public Object getCellEditorValue() {
+        // Get current name and GPA from the table
+        String name = table.getValueAt(row, 0).toString();
+        String gpa = table.getValueAt(row, 1).toString();
+
+        // Open the edit dialog
+        JFrame parent = (JFrame) SwingUtilities.getWindowAncestor(table);
+        EditStudentDialog dialog = new EditStudentDialog(parent, name, gpa);
+        dialog.setVisible(true);
+
+        // If user clicked Save, update the table
+        if (dialog.isSaved()) {
+            table.setValueAt(dialog.getStudentName(), row, 0);
+            table.setValueAt(dialog.getStudentGpa(), row, 1);
+        }
+        return null;
+    }
+}
